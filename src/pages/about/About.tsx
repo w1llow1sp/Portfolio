75b72e64146f7d15d07164bcd2390de3cc6461da
Mary <mari.front.74@gmail.com>
2024-01-19 14:19:45 +0500

    Complete Resume Section

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -7,10 +7,13 @@ import CV from '../../assets/CV.pdf'
 
 import './about.css'
 import Skills from "../../components/Skills/Skills";
+import {resume} from "../../data";
+import ResumeItem from "../../components/Resume/ResumeItem";
 
 const About = () => {
     return (
         <main className="section container">
+            {/* ===SECTION ABOUT START===   */}
             <section className="about">
                 <h2 className="section__title">
                     About <span>Me</span>
@@ -33,9 +36,10 @@ const About = () => {
                     </div>
                 </div>
             </section>
-
             <div className="separator"></div>
+            {/* ===SECTION ABOUT END===   */}
 
+            {/* ===SECTION SKILLS START=== */}
             <section className={'skills'}>
                 <h3 className={'section__subtitle subtitle__center'}>
                     My Skills
@@ -47,8 +51,34 @@ const About = () => {
 
 
             </section>
-
             <div className="separator"></div>
+            {/* ===SECTION SKILLS END=== */}
+
+            {/* ===SECTION RESUME START=== */}
+            <section className="resume">
+                <h3 className={'section__subtitle subtitle__center'}>
+                    Experience & Education
+                </h3>
+                <div className="resume__container grid">
+                    <div className="resume__data">
+                        {resume.map((value) => {
+                            if(value.category==='experience') {
+                                return <ResumeItem key={value.id} {...value}/>
+                            }
+                        })}
+                    </div>
+
+                    <div className="resume__data">
+                        {resume.map((value) => {
+                            if(value.category==='education') {
+                                return <ResumeItem key={value.id} {...value}/>
+                            }
+                        })}
+                    </div>
+                </div>
+            </section>
+            <div className="separator"></div>
+            {/* ===SECTION RESUME END=== */}
         </main>
     );
 };
