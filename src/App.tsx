3d1bb935a206d56d549a5fd660c1cad3f959d1f4
Mary <mari.front.74@gmail.com>
2024-01-24 15:32:48 +0500

    Ready theme and colors togglers

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import About from "./pages/about/About";
 import Portfolio from "./pages/portfolio/Portfolio";
 import Contact from "./pages/contact/Contact";
 import Navbar from "./components/Navbar/Navbar";
+import Themes from "./components/Themes/Themes";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar/>
+        <Themes/>
       <Routes>
         <Route index element={<Home/>}/>
         <Route path={'about'} element={<About/>}/>
---
+++ src/components/Themes/ThemeItem.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type ThemeItemPropsType = {
+    color: string,
+    img: any,
+    changeColor: (color: string) => void
+}
+
+const ThemeItem = ({color, img, changeColor}: ThemeItemPropsType) => {
+    return (
+        <img src={img} alt="" className="theme__img" onClick={()=>changeColor(color)}/>
+    )
+
+
+};
+
+export default ThemeItem;
