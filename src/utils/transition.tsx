c20cf7c0673539b9d70958d74ec4bc3d4745fbbd
Mary <mari.front.74@gmail.com>
2024-01-30 19:20:37 +0500

    Changes some code to better optimize the application

--- src/utils/transition.tsx
+++ src/utils/transition.tsx
@@ -10,14 +10,14 @@ const transition = (OgComponent :FC) => {
                 initial={{scaleY: 0}}
                 animate={{scaleY: 0}}
                 exit={{scaleY: 1}}
-                transition={{duration: 1, ease: [0.22, 1, 0.36, 1]}}
+                transition={{duration: 1, ease: [0.22, 1, 0.15, 1]}}
             />
             <motion.div
                 className={'slide-out'}
                 initial={{scaleY: 1}}
                 animate={{scaleY: 0}}
                 exit={{scaleY: 0}}
-                transition={{duration: 1, ease: [0.22, 1, 0.36, 1]}}
+                transition={{duration: 1, ease: [0.22, 1, 0.15, 1]}}
             />
         </>
     )
